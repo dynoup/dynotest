ef1a612537b9f49af5193965948fe4d0f4f8e114
Seonu <seonup95@gmail.com>
2023-09-14 17:09:19 +0900

    feat: Create Trend component
    Trend, TrendList, TrendItem component 생성

---
+++ src/components/Trend/TrendItem.ts
@@ -0,0 +1,25 @@
+import render from '../../core/render';
+import useCoreComponent from '../../core/useCoreComponent';
+import { ComponentProps } from '../../types/component';
+
+export default function TrendItem(props?: ComponentProps) {
+  const coreComponentProps = useCoreComponent('TrendItem');
+
+  return {
+    ...coreComponentProps,
+    render() {
+      return `
+        <li class="trend-item">
+          <a href="">
+            <p>Trending in South Korea</p>
+            <h3>광역버스</h3>
+            <dl>
+              <dt>Number of posts</dt>
+              <dd>13,030 posts</dd>
+            </dl>
+          </a>
+        </li>
+      `;
+    },
+  };
+}
