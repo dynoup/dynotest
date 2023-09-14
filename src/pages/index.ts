c9ba63135308e29a4264cc9897baa91bfae0da0b
Seonu <seonup95@gmail.com>
2023-09-14 21:16:26 +0900

    fix: Remove unused files

--- src/pages/index.ts
+++
@@ -1,6 +0,0 @@
-import HomePage from './Home';
-import BookmarksPage from './bookmarks';
-import PostPage from './post';
-import ProfilePage from './profile';
-
-export { HomePage, BookmarksPage, PostPage, ProfilePage };
