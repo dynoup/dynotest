2938c3f60df7d028b5e8d9e2af6b23a985d2ff4f
Seonu <seonup95@gmail.com>
2023-09-18 02:51:06 +0900

    refactor: Change name from ***Element to ***Node

--- src/core/diff.ts
+++ src/core/diff.ts
@@ -1,81 +1,81 @@
-// TODO: newElement === null && oldElement 이런식으로 조건문을 작성했을 때 하위에서 newElement가 null일 수도 있다라는 ts 에러가 뜬다. 조건문 deps를 따로 나누는 것으로 해결했는데, 방법이 그거 밖에 없을까?
+// TODO: newNode === null && oldNode 이런식으로 조건문을 작성했을 때 하위에서 newNode가 null일 수도 있다라는 ts 에러가 뜬다. 조건문 deps를 따로 나누는 것으로 해결했는데, 방법이 그거 밖에 없을까?
 export default function updateNode(
-  parentElement: Element,
-  newElement: Element | null,
-  oldElement: Element | null
+  $parentNode: Element,
+  $newNode: Element | null,
+  $oldNode: Element | null
 ) {
-  // 1. oldElement는 있고 newElement는 없는 경우: oldElement를 제거한다.
-  if (newElement === null) {
-    if (oldElement) {
-      oldElement.remove();
+  // 1. oldNode는 있고 newNode는 없는 경우: oldNode를 제거한다.
+  if ($newNode === null) {
+    if ($oldNode) {
+      $oldNode.remove();
     }
     return;
   }
 
-  // 2. oldElement는 없고 newElement는 있는 경우: parentElement에 newElement를 추가한다.
-  if (oldElement === null) {
-    if (newElement) {
-      parentElement.append(newElement);
+  // 2. oldNode는 없고 newNode는 있는 경우: $parentNode에 newNode를 추가한다.
+  if ($oldNode === null) {
+    if ($newNode) {
+      $parentNode.append($newNode);
     }
     return;
   }
 
   // 1. tag 체크
-  // 1-1. oldElement와 newElement가 모두 text node일 경우: text값을 비교하고 다르면 newElement로 교체하고 return 한다.
-  if (oldElement.nodeName === '#text' && newElement.nodeName === '#text') {
-    if (oldElement.textContent !== newElement.textContent) {
-      oldElement.textContent = newElement.textContent;
+  // 1-1. oldNode와 newNode가 모두 text node일 경우: text값을 비교하고 다르면 newNode로 교체하고 return 한다.
+  if ($oldNode.nodeName === '#text' && $newNode.nodeName === '#text') {
+    if ($oldNode.textContent !== $newNode.textContent) {
+      $oldNode.textContent = $newNode.textContent;
     }
 
     return;
   }
 
-  // 1-2. oldElement와 newElement의 tag(nodeName)이 다른 경우: oldElement를 newElement로 교체하고 return 한다.
-  if (oldElement.nodeName !== newElement.nodeName) {
-    oldElement.replaceWith(newElement);
+  // 1-2. oldNode와 newNode의 tag(nodeName)이 다른 경우: oldNode를 newNode로 교체하고 return 한다.
+  if ($oldNode.nodeName !== $newNode.nodeName) {
+    $oldNode.replaceWith($newNode);
     return;
   }
 
   // 3. attribute 체크
-  updateAttributes(oldElement, newElement);
+  updateAttributes($oldNode, $newNode);
 
   // 4. children 재귀
-  const oldElementChildNodes = [...oldElement.childNodes] as Element[];
-  const newElementChildNodes = [...newElement.childNodes] as Element[];
+  const oldNodeChildNodes = [...$oldNode.childNodes] as Element[];
+  const newNodeChildNodes = [...$newNode.childNodes] as Element[];
 
   const totalChildNodesLength = Math.max(
-    oldElementChildNodes.length,
-    newElementChildNodes.length
+    oldNodeChildNodes.length,
+    newNodeChildNodes.length
   );
 
   for (let i = 0; i < totalChildNodesLength; i++) {
     updateNode(
-      oldElement,
-      newElementChildNodes[i] || null,
-      oldElementChildNodes[i] || null
+      $oldNode,
+      newNodeChildNodes[i] || null,
+      oldNodeChildNodes[i] || null
     );
   }
 }
 
 // TODO: property(ex: checked)도 업데이트도 해주기. (필요한지 체크 해보고)
-function updateAttributes(oldElement: Element, newElement: Element) {
-  const oldElementAttributes = [...oldElement.attributes]; // {key: value}[]
-  const newElementAttributes = [...newElement.attributes];
+function updateAttributes($oldNode: Element, $newNode: Element) {
+  const oldNodeAttributes = [...$oldNode.attributes]; // {key: value}[]
+  const newNodeAttributes = [...$newNode.attributes];
 
-  for (const { name, value } of newElementAttributes) {
-    // 1. newElement에 있고 oldElement에 있는데 값이 같음
-    if (value === oldElement.getAttribute(name)) continue;
+  for (const { name, value } of newNodeAttributes) {
+    // 1. newNode에 있고 oldNode에 있는데 값이 같음
+    if (value === $oldNode.getAttribute(name)) continue;
 
-    // 2. newElement에 있고 oldElement에 없음
-    // 3. newElement에 있고 oldElement에 있는데 값이 다름
-    oldElement.setAttribute(name, value);
+    // 2. newNode에 있고 oldNode에 없음
+    // 3. newNode에 있고 oldNode에 있는데 값이 다름
+    $oldNode.setAttribute(name, value);
   }
 
-  for (const { name, value } of oldElementAttributes) {
-    // 1. oldElement에 있고 newElement에 있는데 값이 같음
-    if (value === newElement.getAttribute(name)) continue;
+  for (const { name, value } of oldNodeAttributes) {
+    // 1. oldNode에 있고 newNode에 있는데 값이 같음
+    if (value === $newNode.getAttribute(name)) continue;
 
-    // 2. oldElement에 있고 newElement에 없음
-    if (!newElement.hasAttribute(name)) oldElement.removeAttribute(name);
+    // 2. oldNode에 있고 newNode에 없음
+    if (!$newNode.hasAttribute(name)) $oldNode.removeAttribute(name);
   }
 }
