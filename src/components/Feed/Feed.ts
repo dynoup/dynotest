2c30ce72c386f1c50f14d1a6bd7bab0718ab36e2
Seonu <seonup95@gmail.com>
2023-09-21 02:18:46 +0900

    feat: Apply context in HomePage, Feed Component

--- src/components/Feed/Feed.ts
+++ src/components/Feed/Feed.ts
@@ -1,15 +1,37 @@
-import render from '../../core/render';
-import useCoreComponent from '../../core/useCoreComponent';
-import { ComponentProps } from '../../types/core/component';
+import { ComponentProps } from '../../packages/core/component.type';
+import useCoreComponent from '../../packages/core/useCoreComponent';
+import { setEvents } from '../../packages/event/event';
+import { useContext } from '../../packages/store';
 
 export default function Feed(props?: ComponentProps) {
-  const componentObject = useCoreComponent({ component: Feed });
+  const componentObject = useCoreComponent({
+    component: Feed,
+    parent: props?.parent,
+  });
+  // TODO: 호출하는 쪽에서 제너릭으로 타입 지정하도록 만들기
+  const [getCount, setCount] = useContext('count', () => componentObject);
+
+  function handleIncrementCount() {
+    console.log('[handleIncrementCount]');
+    setCount((count: number) => count + 1);
+  }
+
+  setEvents([
+    {
+      componentKey: componentObject.key,
+      selector: '.increment-count-button',
+      action: 'click',
+      handler: handleIncrementCount,
+    },
+  ]);
 
   componentObject.render = () => {
     return `
-        <h3>Feed</h3>
-      `;
+      <h3>Feed</h3>
+      <p>count: ${getCount()}</p>
+      <button type="button" class="increment-count-button">Count up</button>
+    `;
   };
 
-  return componentObject;
+  return () => componentObject;
 }
--- src/pages/home.ts
+++ src/pages/home.ts
@@ -1,10 +1,16 @@
 import Feed from '../components/Feed/Feed';
-import render from '../core/render';
-import useCoreComponent from '../core/useCoreComponent';
-import { ComponentProps } from '../types/core/component';
+import { CountContext } from '../context/Context';
+import { ComponentProps } from '../packages/core/component.type';
+import render from '../packages/core/render';
+import useCoreComponent from '../packages/core/useCoreComponent';
 
 export default function HomePage(props?: ComponentProps) {
   const componentObject = useCoreComponent({ component: HomePage });
+
+  CountContext(() => componentObject);
+
+  // TODO: parent 이렇게 전달 안하는 방법... 알아보기
+  const FeedInstance = Feed({ parentComponent: componentObject });
 
   componentObject.render = () => {
     return `
@@ -14,7 +20,7 @@ export default function HomePage(props?: ComponentProps) {
           </header>
         
           <div class="section-content">
-            ${render(Feed)}
+            ${render(FeedInstance)}
           </div>
         </section>
       `;
