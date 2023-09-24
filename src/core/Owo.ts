f54ba139596056aca050e1360611c014cc4deba0
Owon <chlehdnjs@gmail.com>
2023-09-25 02:02:37 +0900

    style: add base style

--- src/core/Owo.ts
+++ src/core/Owo.ts
@@ -2,5 +2,10 @@ export function createRoot() {
   const element = document.createElement('div');
   element.id = '__App';
   document.body.appendChild(element);
+  // 초기 css 값
+  element.style.cssText = 'display: flex; width: 100vw; height: 100vh;';
+  // body css 값 제거
+  document.body.style.cssText =
+    'margin: 0; background-color: #212121; color: #FFFFFF;';
   return element;
 }
