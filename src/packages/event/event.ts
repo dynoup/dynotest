4de83683b3de70894bf1de3ccdf4d96037d10b90
Seonu <seonup95@gmail.com>
2023-09-27 18:47:53 +0900

    refactor: Change to filter duplcation event

--- src/packages/event/event.ts
+++ src/packages/event/event.ts
@@ -3,7 +3,6 @@ import { EventObject } from './event.type';
 export const events: EventObject[] = [];
 
 export function setEvents(newEvents: EventObject[]) {
-  // TODO: handler는 컴포넌트가 렌더링될 때마다 참조값이 바뀔텐데, 이걸 어떻게 동일한 handler로 인지하게 할 수 있을까? deep copy?? 함수를 컴포넌트 밖에서 선언?
   const newDeduplicatedEvents = newEvents.filter(
     ({ componentKey, selector, action, handler }) => {
       return !events.some(
@@ -11,7 +10,7 @@ export function setEvents(newEvents: EventObject[]) {
           event.componentKey === componentKey &&
           event.selector === selector &&
           event.action === action &&
-          event.handler === handler
+          event.handler.name === handler.name
       );
     }
   );
