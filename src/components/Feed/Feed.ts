ed05eec1bbd4654ad6aa1b23ce6d998eee037ac4
Seonu <seonup95@gmail.com>
2023-09-27 18:50:35 +0900

    feat: Implement Feed with fetch data

--- src/components/Feed/Feed.ts
+++ src/components/Feed/Feed.ts
@@ -1,7 +1,15 @@
-import { ComponentProps } from '../../packages/core/component.type';
+import { fetchData } from '../../api/fetch';
+import {
+  ComponentInstance,
+  ComponentProps,
+} from '../../packages/core/component.type';
+import render from '../../packages/core/render';
 import useCoreComponent from '../../packages/core/useCoreComponent';
 import { setEvents } from '../../packages/event/event';
+import useState from '../../packages/state/useState';
 import { useContext } from '../../packages/store';
+import { FeedItemData } from './Feed.type';
+import FeedItem from './FeedItem';
 
 export default function Feed(props?: ComponentProps) {
   const componentObject = useCoreComponent({
@@ -10,9 +18,20 @@ export default function Feed(props?: ComponentProps) {
   });
   // TODO: 호출하는 쪽에서 제너릭으로 타입 지정하도록 만들기
   const [getCount, setCount] = useContext('count', () => componentObject);
+  const [posts, setPosts] = useState(componentObject, 'posts', []);
+  let FeedItemInstances: ComponentInstance[] = [];
+
+  fetchData('/api/posts', 'GET', {
+    onSuccess(data) {
+      FeedItemInstances = data.posts.map((post: FeedItemData) =>
+        FeedItem({ parent: componentObject, post })
+      );
+      console.log('[data]', data);
+      setPosts(data.posts);
+    },
+  });
 
   function handleIncrementCount() {
-    console.log('[handleIncrementCount]');
     setCount((count: number) => count + 1);
   }
 
@@ -30,6 +49,12 @@ export default function Feed(props?: ComponentProps) {
       <h3>Feed</h3>
       <p>count: ${getCount()}</p>
       <button type="button" class="increment-count-button">Count up</button>
+
+      <div>
+        ${FeedItemInstances.map((FeedItemInstance) =>
+          render(FeedItemInstance)
+        ).join('')}
+      </div>
     `;
   };
 
---
+++ src/components/Feed/Feed.type.ts
@@ -0,0 +1,9 @@
+export interface FeedItemData {
+  userName: string;
+  userId: string;
+  postId: string;
+  profileUrl: string;
+  contents: string;
+  imageUrl: string;
+  isBookmarked: boolean;
+}
